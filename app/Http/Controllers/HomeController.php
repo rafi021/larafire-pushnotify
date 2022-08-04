8015bd4f77ae359fcb35d7b36e4be06ace0ab341
rafi021 <mahmud.ibrahim021@gmail.com>
2022-08-04 11:52:42 +0600

    add route and update controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -25,4 +25,22 @@ class HomeController extends Controller
     {
         return view('home');
     }
+
+    public function updateToken(Request $request)
+    {
+        try {
+            $request->user()->update([
+                'fcm_token' => $request->token
+            ]);
+
+            return response()->json([
+                'success' => true
+            ]);
+        } catch (\Exception $e) {
+            report($e);
+            return response()->json([
+                'success' => false
+            ], 500);
+        }
+    }
 }
